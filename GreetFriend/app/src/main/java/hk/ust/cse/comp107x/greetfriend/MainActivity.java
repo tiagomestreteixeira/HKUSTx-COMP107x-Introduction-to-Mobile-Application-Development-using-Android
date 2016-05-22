4ae6f933ee068d95c8cce028d216cd6262258b65
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-05-22 17:48:51 +0100

    Added the correct behaviour to the greetings button, and update the greetings TextView according

--- GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
+++ GreetFriend/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
@@ -4,14 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    Button greetButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        greetButton = (Button) findViewById(R.id.greetButton);
+        greetButton.setOnClickListener(this);
     }
 
     @Override
@@ -34,5 +42,22 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView textMessage = (TextView) findViewById(R.id.textMessage);
+
+        EditText editFriendName = (EditText) findViewById(R.id.editFriendName);
+
+        String friendName = editFriendName.getText().toString();
+
+        switch (view.getId()){
+            case R.id.greetButton:
+                textMessage.setText(getString(R.string.greetstring) + friendName + "!");
+                break;
+            default:
+                break;
+        }
     }
 }
