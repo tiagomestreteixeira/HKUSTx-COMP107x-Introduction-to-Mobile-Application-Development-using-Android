5946c9599dd9fc42503b94e7d5e71a54bd40668e
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-05-25 16:08:17 +0100

    Added Activity Lifecycle overriding methods and logging info

--- GreetFriend-Week2/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
+++ GreetFriend-Week2/app/src/main/java/hk/ust/cse/comp107x/greetfriend/MainActivity.java
@@ -24,7 +24,45 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         names = getResources().getStringArray(R.array.friends);
         setListAdapter(new ArrayAdapter<>(this,R.layout.friend_item,names));
+        Log.i(TAG,"in onCreate()");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"in onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"in onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"in onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"in onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"in onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"in onDestroy()");
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- GreetFriend-Week2/app/src/main/java/hk/ust/cse/comp107x/greetfriend/ShowMessage.java
+++ GreetFriend-Week2/app/src/main/java/hk/ust/cse/comp107x/greetfriend/ShowMessage.java
@@ -3,10 +3,13 @@ package hk.ust.cse.comp107x.greetfriend;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class ShowMessage extends Activity {
 
+
+    private static final String TAG = "ShowMessageactivity";
     private String message;
 
     @Override
@@ -19,6 +22,42 @@ public class ShowMessage extends Activity {
 
         TextView tv = (TextView)findViewById(R.id.textMessage);
         tv.setText(message);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG,"in onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,"in onRestart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,"in onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,"in onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG,"in onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"in onDestroy()");
     }
 
 }
