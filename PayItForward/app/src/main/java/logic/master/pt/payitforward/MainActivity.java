7bad3102a8d8b21866011fe0cbb5b6f0cda98e51
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-05-29 02:53:40 +0100

    Initial Commit of PayItForwardCalculator

---
+++ PayItForward/app/src/main/java/logic/master/pt/payitforward/MainActivity.java
@@ -0,0 +1,85 @@
+package logic.master.pt.payitforward;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mNumberActions;
+    private int numberActions;
+
+    private EditText mNumberDepth;
+    private int numberDepth;
+
+    private Button mCalculate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mNumberActions = (EditText) findViewById(R.id.numberActionPerPerson);
+        mNumberDepth = (EditText) findViewById(R.id.numberDepth);
+        mCalculate = (Button) findViewById(R.id.buttonCalculate);
+        mCalculate.setEnabled(false);
+        //numberActions = Integer.parseInt(mNumberActions.getText().toString());
+        enableViewOnNonEmptyEditText(mNumberActions,);
+    }
+
+    private void enableViewOnNonEmptyEditText(EditText editText, View view){
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //numberActions = Integer.parseInt(mNumberActions.getText().toString());
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
