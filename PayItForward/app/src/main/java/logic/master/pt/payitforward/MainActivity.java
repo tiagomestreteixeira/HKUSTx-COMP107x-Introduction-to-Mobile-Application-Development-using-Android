d254daa699073a59f6cbf20fbf8698de0656d472
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-06-04 00:38:29 +0100

    Removed unused imports regarding Menu

--- PayItForward/app/src/main/java/logic/master/pt/payitforward/MainActivity.java
+++ PayItForward/app/src/main/java/logic/master/pt/payitforward/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
